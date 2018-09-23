4bc711469ae5891e67f4600717fff2255f3190bb
haonx <qtvhao@gmail.com>
2018-09-23 13:56:30 +0700

    add method setEnd()

---
+++ src/DateTime.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/23/2018
+ * Time: 1:55 PM
+ */
+
+namespace Qtvhao\CalendarSchedule;
+
+
+use Carbon\Carbon;
+
+class DateTime extends Carbon{
+
+}
